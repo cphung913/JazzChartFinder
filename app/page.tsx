39b585adc8b5cce5af95b8b2440a1a88a433f0e8
Chase Phung <chasephung13@gmail.com>
2026-02-01 00:06:03 -0800

    connect Neon PostgreSQL

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-import Image from "next/image";
+export default async function Home() {
+  // const DATABASE_URL = process.env.DATABASE_URL as string;
+  // const res = await fetch(DATABASE_URL);
+  // const data = await res.json();
+  // console.log(data);
 
-export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
